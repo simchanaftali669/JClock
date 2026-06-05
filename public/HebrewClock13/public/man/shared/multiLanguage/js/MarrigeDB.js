9a47ef78c011b675cd6b24f7bfe5b781f7f1812b
Naftali Bilig <simchanaftali669@gmail.com>
2026-06-05 06:11:01 +0300

    add yaki

--- public/HebrewClock13/public/man/shared/multiLanguage/js/MarrigeDB.js
+++ public/HebrewClock13/public/man/shared/multiLanguage/js/MarrigeDB.js
@@ -1,32 +1,20 @@
 //{ID : husband ID, hebrewDay: married man/woman hebrew day of born, hebrewHour: married man/woman hebrew hour of the day/night (1-24)
 var marrigeDB = [
-				 {ID : "חנה לאה ביליג", hebrewDay: 06, hebrewHour: 09},   
-				 {ID : "נפתלי ביליג", hebrewDay: 05, hebrewHour: 03},  
-				 {ID : "Naftali Bilig", hebrewDay: 05, hebrewHour: 03},				 
-				 {ID : "אוריה עליה ביליג", hebrewDay: 06, hebrewHour: 21}, 
-				 {ID : "נהוראי ביליג", hebrewDay: 04, hebrewHour: 17},     
-				 {ID : "ראובן ביליג", hebrewDay: 06, hebrewHour: 15},			
-				 {ID : "צפורה ביליג", hebrewDay: 04, hebrewHour: 15},
-				 {ID : "אילה ביליג", hebrewDay: 06, hebrewHour: 17},
-				 {ID : "יעקב ישראל ביליג", hebrewDay: 01, hebrewHour: 13},
-				 
-				 {ID : "אביעד אברהם ביליג", hebrewDay: 06, hebrewHour: 11},
-				 {ID : "תהילה ביליג", hebrewDay: 06, hebrewHour: 12},
-				 {ID : "ערבה זהר ביליג", hebrewDay: 07, hebrewHour: 02},
-				 {ID : "רוני ביליג", hebrewDay: 07, hebrewHour: 10},
-				 {ID : "Roni Bilig", hebrewDay: 07, hebrewHour: 10},
-				 {ID : "אודיה ביליג", hebrewDay: 03, hebrewHour: 13},
-				 
-				 {ID : "אופיר כהן", hebrewDay: 04, hebrewHour: 20},					 
-				 {ID : "מאיר גזית", hebrewDay: 07, hebrewHour: 20},
-				 {ID : "חיים הדר אליהו", hebrewDay: 07, hebrewHour: 10},			     
-				 {ID : "יזהר לב", hebrewDay: 05, hebrewHour: 14},
-				 {ID : "Littlepeach", hebrewDay: 04, hebrewHour: 03},
-				 
-				 {ID : "חגי רצבי", hebrewDay: 04, hebrewHour: 23},
-				 {ID : "Hagay Ratzabi", hebrewDay: 04, hebrewHour: 23},
-				 {ID : "יחיאל אשר בר יהודה", hebrewDay: 06, hebrewHour: 08},
-				 {ID : "חדוה איילה בר יהודה", hebrewDay: 07, hebrewHour: 02}
-				 
-				// {ID : "יסקה צופיה ביליג", hebrewDay: 06, hebrewHour: 12}  
+				 {ID : "חנה לאה ביליג", hebrewDay: 06, hebrewHour: 09, mazalHour: 06},   
+				 {ID : "נפתלי ביליג", hebrewDay: 04, hebrewHour: 23, mazalHour: 01},
+				 {ID : "Naftali Bilig", hebrewDay: 04, hebrewHour: 23, mazalHour: 01},
+				 {ID : "אוריה עליה ביליג", hebrewDay: 06, hebrewHour: 21, mazalHour: 06}, 
+				 {ID : "נהוראי ביליג", hebrewDay: 03, hebrewHour: 22, mazalHour: 05},     
+
+				 {ID : "אביעד אברהם ביליג", hebrewDay: 04, hebrewHour: 23, mazalHour: 01},
+				 {ID : "יעקב ישראל ביליג", hebrewDay: 07, hebrewHour: 15, mazalHour: 02},
+
+				 {ID : "ראובן ביליג", hebrewDay: 06, hebrewHour: 07	, mazalHour: 06},			
+				 {ID : "צפורה ביליג", hebrewDay: 04, hebrewHour: 15ת mazalHour: 04},
+				 {ID : "אילה ביליג", hebrewDay: 06, hebrewHour: 17, mazalHour: 01},
+				 {ID : "תהילה ביליג", hebrewDay: 06, hebrewHour: 12, mazalHour: 03},
+				 {ID : "ערבה זהר ביליג", hebrewDay: 07, hebrewHour: 02, mazalHour: 03},
+				 {ID : "רוני ביליג", hebrewDay: 07, hebrewHour: 10, mazalHour: 03},
+				 {ID : "Roni Bilig", hebrewDay: 07, hebrewHour: 10, mazalHour: 03},
+				 {ID : "אודיה ביליג", hebrewDay: 03, hebrewHour: 13, mazalHour: 02},
 				]
